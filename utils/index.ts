c5e20e6671cdcbbbb5dcb914039943053965e5d1
wal33dahmad <rana.waleed123@gmail.com>
2023-07-15 03:06:08 +0500

    car details modal

--- utils/index.ts
+++ utils/index.ts
@@ -36,3 +36,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return RentPerDay.toFixed(0);
 };
+
+// export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+//   // key hrjavascript-mastery
+
+//   const url = new URL("https://cdn.imagin.studio/getimage");
+
+//   const { make, model, year } = car;
+
+//   url.searchParams.append("customer", "hrjavascript-mastery");
+//   url.searchParams.append("make", make);
+//   url.searchParams.append("modelFamily", model.split(" ")[0]);
+//   url.searchParams.append("zoomType", "fullscreen");
+//   url.searchParams.append("modelYear", `${year}`);
+//   url.searchParams.append("angle", `${angle}`);
+
+//   return `${url}`;
+// };
